569920a368c90e2eac2386a446a768c4bfc1d333
GuilleDevLubuntu <guillepower2001@gmail.com>
2026-07-06 11:35:49 -0600

    Creacion dto de categoria

---
+++ backend/src/main/java/com/guilledev/backend/categoria/dto/CategoriaRequest.java
@@ -0,0 +1,11 @@
+package com.guilledev.backend.categoria.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoriaRequest {
+    private String nombre;
+    private String descripcion;
+}
