9763153156c89ee8052c2b560b0a8f735028ef5a
GuilleDevLubuntu <guillepower2001@gmail.com>
2026-07-06 11:28:48 -0600

    Creacion repository de categoria

---
+++ backend/src/main/java/com/guilledev/backend/categoria/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.guilledev.backend.categoria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guilledev.backend.categoria.entity.Categoria;
+
+public interface CategoriaRepository 
+    extends JpaRepository<Categoria, Long>{
+        
+}
