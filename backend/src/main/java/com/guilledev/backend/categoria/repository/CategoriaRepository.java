2cfe427efecce8704c02996d280d95d47bdb19bb
GuilleDevLubuntu <guillepower2001@gmail.com>
2026-07-06 19:38:28 -0600

    Creacion del metodo save del serviceimpl y se agregaron metodos en el categoria repository

--- backend/src/main/java/com/guilledev/backend/categoria/repository/CategoriaRepository.java
+++ backend/src/main/java/com/guilledev/backend/categoria/repository/CategoriaRepository.java
@@ -1,10 +1,12 @@
 package com.guilledev.backend.categoria.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guilledev.backend.categoria.entity.Categoria;
 
-public interface CategoriaRepository 
-    extends JpaRepository<Categoria, Long>{
-        
+public interface CategoriaRepository
+        extends JpaRepository<Categoria, Long> {
+    boolean existsByNombre(String nombre);
 }
--- backend/src/main/java/com/guilledev/backend/categoria/service/CategoriaServiceImpl.java
+++ backend/src/main/java/com/guilledev/backend/categoria/service/CategoriaServiceImpl.java
@@ -1,11 +1,13 @@
 package com.guilledev.backend.categoria.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.guilledev.backend.categoria.dto.CategoriaRequest;
 import com.guilledev.backend.categoria.dto.CategoriaResponse;
+import com.guilledev.backend.categoria.entity.Categoria;
 import com.guilledev.backend.categoria.mapper.CategoriaMapper;
 import com.guilledev.backend.categoria.repository.CategoriaRepository;
 
@@ -13,16 +15,33 @@ import com.guilledev.backend.categoria.repository.CategoriaRepository;
 public class CategoriaServiceImpl implements CategoriaService {
 
     private final CategoriaRepository repository;
-    private final CategoriaMapper mapper;    
-    
+    private final CategoriaMapper mapper;
+
     public CategoriaServiceImpl(CategoriaRepository repository, CategoriaMapper mapper) {
         this.repository = repository;
         this.mapper = mapper;
     }
 
+    /*
+     * Método guardar categoria, primero convierte el request en una entidad, de ahi
+     * como id es null con save comunicamos
+     * con hibernate y ve el null y hace un insert e hibernate toma ese id y lo
+     * coloca dentro de la entidad(objeto), al final
+     * convertimos la entidad en response para no pasar toda la entidad al cliente y
+     * retornamos el response
+     */
     @Override
     public CategoriaResponse guardar(CategoriaRequest request) {
-        throw new UnsupportedOperationException("Unimplemented method 'guardar'");
+        
+        if (repository.existsByNombre(request.getNombre())) {
+            throw new RuntimeException("La categoría ya existe");
+        }
+
+        Categoria categoria = mapper.toEntity(request);
+        Categoria categoriaGuardada = repository.save(categoria);
+        CategoriaResponse response = mapper.toResponse(categoriaGuardada);
+
+        return response;
     }
 
     @Override
