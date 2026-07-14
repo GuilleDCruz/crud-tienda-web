d97db985cb290875178168ccf42cf390352cc3ad
GuilleDevWin <guillepower2001@gmail.com>
2026-07-13 18:05:44 -0600

    Se agregaron los metodos de controller (pendiente del postman)

---
+++ backend/src/main/java/com/guilledev/backend/categoria/controller/CategoriaController.java
@@ -0,0 +1,65 @@
+package com.guilledev.backend.categoria.controller;
+
+import com.guilledev.backend.categoria.service.CategoriaService;
+import com.guilledev.backend.categoria.service.CategoriaServiceImpl;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilledev.backend.categoria.dto.CategoriaRequest;
+import com.guilledev.backend.categoria.dto.CategoriaResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/categoria/")
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    // Crear
+    @PostMapping
+    public CategoriaResponse guardar(@RequestBody CategoriaRequest request) {
+        return categoriaService.guardar(request);
+    }
+
+    // Actualizar
+    @PutMapping("/{id}")
+    public CategoriaResponse actualizar(@PathVariable Long id, @RequestBody CategoriaRequest request) {
+        return categoriaService.actualizar(id, request);
+    }
+
+    // Listar
+    @GetMapping
+    public List<CategoriaResponse> listar() {
+        return categoriaService.listar();
+    }
+
+    // Listar por categoria
+    @GetMapping("/{id}")
+    public CategoriaResponse buscarPorID(@PathVariable Long id) {
+        return categoriaService.buscarPorID(id);
+    }
+
+    // Eliminar
+    @DeleteMapping("path/{id}")
+    public String eliminar(@PathVariable Long id) {
+        categoriaService.eliminar(id);
+
+        return "Categoría con '" + id + "' eliminada correctamente";
+    }
+
+}
