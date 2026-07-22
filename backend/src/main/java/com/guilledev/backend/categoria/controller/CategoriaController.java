ce27a1b56bff15dc3e1bc6f62247b980d5776907
GuilleDevWin <guillepower2001@gmail.com>
2026-07-22 12:47:36 -0600

    Implementacion de Swagger para la documentacion de la api

--- backend/src/main/java/com/guilledev/backend/categoria/controller/CategoriaController.java
+++ backend/src/main/java/com/guilledev/backend/categoria/controller/CategoriaController.java
@@ -2,6 +2,10 @@ package com.guilledev.backend.categoria.controller;
 
 import com.guilledev.backend.categoria.service.CategoriaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +26,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/api/categoria/")
+@RequestMapping("/api/categoria")
+@Tag(name = "Categoría", description = "Operaciones relacionadas con la gestión de categorías")
 public class CategoriaController {
 
     private final CategoriaService categoriaService;
@@ -32,31 +37,55 @@ public class CategoriaController {
     }
 
     // Crear
+    @Operation(summary = "Crear una categoría", description = "Crea una nueva categoría en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoría creada correctamente"),
+            @ApiResponse(responseCode = "400", description = "Los datos enviados no son válidos"),
+            @ApiResponse(responseCode = "409", description = "La categoría ya existe")
+    })
+
     @PostMapping
     public CategoriaResponse guardar(@Valid @RequestBody CategoriaRequest request) {
         return categoriaService.guardar(request);
     }
 
     // Actualizar
-    @PutMapping("/{id}")
+    @Operation(summary = "Actualizar una categoría", description = "Actualiza los datos de una categoría existente y activa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoría actualizada correctamente"),
+            @ApiResponse(responseCode = "404", description = "La categoría no existe")
+    })
+    @PutMapping("{id}")
     public CategoriaResponse actualizar(@Valid @PathVariable Long id, @RequestBody CategoriaRequest request) {
         return categoriaService.actualizar(id, request);
     }
 
     // Listar
+    @Operation(summary = "Listar categorías", description = "Obtiene todas las categorías registradas")
+    @ApiResponse(responseCode = "200", description = "Categorías obtenidas correctamente")
     @GetMapping
     public List<CategoriaResponse> listar() {
         return categoriaService.listar();
     }
 
-    // Listar por categoria
-    @GetMapping("/{id}")
+    // Listar por Id
+    @Operation(summary = "Buscar categoría por Id", description = "Obtiene una categoría específica por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoría encontrada"),
+            @ApiResponse(responseCode = "404", description = "La categoría no existe")
+    })
+    @GetMapping("{id}")
     public CategoriaResponse buscarPorID(@PathVariable Long id) {
         return categoriaService.buscarPorID(id);
     }
 
     // Eliminar
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar una categoría", description = "Desactiva una categoría existente mediante su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoría eliminada correctamente"),
+            @ApiResponse(responseCode = "404", description = "La categoría no existe")
+    })
+    @DeleteMapping("{id}")
     public String eliminar(@PathVariable Long id) {
         categoriaService.eliminar(id);
 
